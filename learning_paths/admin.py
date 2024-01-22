7baa52668fa6e9876d04372bf51f28271bce7b3c
Artur Gaspar <artur@opencraft.com>
2024-01-22 04:28:35 -0300

    feat: allow selecting courses for Learning Path steps in admin

--- learning_paths/admin.py
+++ learning_paths/admin.py
@@ -1,15 +1,35 @@
 """
 Django Admin for learning_paths.
 """
+from django import forms
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
+from .compat import get_course_keys_with_outlines
 from .models import AcquiredSkill, LearningPath, LearningPathStep, RequiredSkill, Skill
+
+
+def get_course_keys_choices():
+    """Get course keys in an adequate format for a choice field."""
+    yield None, ""
+    for key in get_course_keys_with_outlines():
+        yield key, key
+
+
+class LearningPathStepForm(forms.ModelForm):
+    """Admin form for Learning Path step."""
+
+    # TODO: Use autocomplete select instead.
+    # See <https://github.com/open-craft/section-to-course/blob/db6fd6f8f4478e91bb531e6c2fa50143e1c2e012/
+    #      section_to_course/admin.py#L31-L140>
+    course_key = forms.ChoiceField(choices=get_course_keys_choices, label=_("Course"))
 
 
 class LearningPathStepInline(admin.TabularInline):
     """Inline Admin for Learning Path step."""
 
     model = LearningPathStep
+    form = LearningPathStepForm
 
 
 class AcquiredSkillInline(admin.TabularInline):
