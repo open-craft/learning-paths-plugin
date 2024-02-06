f4369f70e5b4103629b816f4f961d94d911a7f33
Sid Verma <sid@opencraft.com>
2024-02-06 23:57:51 +0530

    feat: add Enrollment model and ability to bulk enroll

--- learning_paths/admin.py
+++ learning_paths/admin.py
@@ -3,11 +3,22 @@ Django Admin for learning_paths.
 """
 
 from django import forms
-from django.contrib import admin
+from django.contrib import admin, auth
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.utils.translation import gettext_lazy as _
 
 from .compat import get_course_keys_with_outlines
-from .models import AcquiredSkill, LearningPath, LearningPathStep, RequiredSkill, Skill
+from .models import (
+    AcquiredSkill,
+    LearningPath,
+    LearningPathEnrollment,
+    LearningPathStep,
+    RequiredSkill,
+    Skill,
+)
+
+User = auth.get_user_model()
 
 
 def get_course_keys_choices():
@@ -45,10 +56,43 @@ class RequiredSkillInline(admin.TabularInline):
     model = RequiredSkill
 
 
+class BulkEnrollUsersForm(forms.ModelForm):
+    """Form to bulk enroll users in a learning path."""
+
+    usernames = forms.CharField(
+        widget=forms.Textarea,
+        help_text="Enter usernames separated by newlines",
+        label="Bulk enroll users",
+        required=False,
+    )
+
+    class Meta:
+        """Form options."""
+
+        model = LearningPath
+        fields = "__all__"
+
+    def clean_usernames(self):
+        """Validate usernames and return a list of users."""
+        data = self.cleaned_data["usernames"]
+        if not data:
+            return []
+        usernames = [username.strip() for username in data.split("\n")]
+        users = User.objects.filter(username__in=usernames)
+        found_usernames = list(users.values_list("username", flat=True))
+        invalid_usernames = set(usernames) - set(found_usernames)
+        if invalid_usernames:
+            raise ValidationError(
+                f"The following usernames are not valid: {', '.join(invalid_usernames)}"
+            )
+        return users
+
+
 class LearningPathAdmin(admin.ModelAdmin):
     """Admin for Learning Path."""
 
     model = LearningPath
+    form = BulkEnrollUsersForm
 
     search_fields = [
         "slug",
@@ -68,6 +112,15 @@ class LearningPathAdmin(admin.ModelAdmin):
         AcquiredSkillInline,
     ]
 
+    def save_related(self, request, form, formsets, change):
+        """Save related objects and enroll users in the learning path."""
+        super().save_related(request, form, formsets, change)
+        with transaction.atomic():
+            for user in form.cleaned_data["usernames"]:
+                LearningPathEnrollment.objects.get_or_create(
+                    user=user, learning_path=form.instance
+                )
+
 
 class SkillAdmin(admin.ModelAdmin):
     """Admin for Learning Path generic skill."""
@@ -75,5 +128,20 @@ class SkillAdmin(admin.ModelAdmin):
     model = Skill
 
 
+class EnrolledUsersAdmin(admin.ModelAdmin):
+    """Admin for Learning Path enrollment."""
+
+    model = LearningPathEnrollment
+
+    search_fields = [
+        "id",
+        "user__username",
+        "learning_path__uuid",
+        "learning_path__slug",
+        "learning_path__display_name",
+    ]
+
+
 admin.site.register(LearningPath, LearningPathAdmin)
 admin.site.register(Skill, SkillAdmin)
+admin.site.register(LearningPathEnrollment, EnrolledUsersAdmin)
